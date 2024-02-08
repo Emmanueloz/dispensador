07389d35d43ec34db6abb32c6fb9d62571e3d271
EmmanuelOZ <davidozuna84@hotmail.com>
2024-02-07 18:12:29 -0600

    codigo de uso en el crud eliminado

--- dispensador_python/crud.py
+++ dispensador_python/crud.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 class Crud:
     def __init__(self):
         self.conexion = self.conectar_BD()
@@ -24,15 +25,13 @@ class Crud:
             consulta = self.conexion.cursor()
             consulta.execute(instruccion, (idSensor,))
             resultado = ""
-            for (idSensor,nombre, uso) in consulta:
-                resultado += "{}\t{}\t{}\n".format(idSensor,nombre, uso)
+            for (idSensor, nombre, uso) in consulta:
+                resultado += "{}\t{}\t{}\n".format(idSensor, nombre, uso)
             consulta.close()
             return resultado if resultado else "No se encontraron resultados."
         except mysql.connector.Error as error:
             return f"Error al consultar la base de datos: {error}"
 
-
-    
     def consultar_registros(self, tabla):
         try:
             instruccion = f"SELECT * FROM {tabla}"
@@ -40,7 +39,8 @@ class Crud:
             consulta.execute(instruccion)
             resultado = ""
             for (idRegistro, estado, fecha, hora) in consulta:
-                resultado += "{}\t{}\t{}\t{}\n".format(idRegistro, estado, fecha, hora)
+                resultado += "{}\t{}\t{}\t{}\n".format(
+                    idRegistro, estado, fecha, hora)
             consulta.close()
             return resultado if resultado else "No se encontraron resultados."
         except mysql.connector.Error as error:
@@ -48,7 +48,7 @@ class Crud:
 
     def insertar_registro(self, tabla, idRegistro, estado, fecha, hora):
         try:
-            instruccion = f'''INSERT INTO {tabla} (idRegistro, estado, fecha, hora) 
+            instruccion = f'''INSERT INTO {tabla} (idRegistro, estado, fecha, hora)
                               VALUES (%s, %s, %s, %s)'''
             valores = (idRegistro, estado, fecha, hora)
             consulta = self.conexion.cursor()
@@ -61,7 +61,7 @@ class Crud:
 
     def insertar_sensor(self, tabla, idSensor, nombre, uso):
         try:
-            instruccion = f'''INSERT INTO {tabla} (idSensor, nombre, uso) 
+            instruccion = f'''INSERT INTO {tabla} (idSensor, nombre, uso)
                               VALUES (%s, %s, %s)'''
             valores = (idSensor, nombre, uso)
             consulta = self.conexion.cursor()
@@ -71,24 +71,3 @@ class Crud:
             return "Sensor insertado correctamente."
         except mysql.connector.Error as error:
             return f"Error al insertar en la tabla: {error}"
-
-
-
-crud = Crud()
-
-# Consultar sensores
-#resultado = crud.consultar_sensor("sensores", "nombre_del_sensor")
-print(crud.consultar_sensor("Sensores", 1))
-#print(resultado)
-
-# Consultar registros
-#print(crud.consultar_registros("Registros"))
-
-# Insertar un nuevo sensor
-#print(crud.insertar_sensor("Sensores", 2, "nombre_del_sensor", "uso_del_sensor"))
-
-# Insertar un nuevo registro
-#print(crud.insertar_registro("Registros", 2, "estado_del_registro", "2024-02-07", "12:00:00"))
-
-# Cerrar la conexión
-print(crud.cerrar_conexion())
