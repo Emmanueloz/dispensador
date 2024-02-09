72a2c27c2375bbae9f24e520b2c46d4437694df7
alexander1939 <alexander1939>
2024-02-09 10:27:12 -0600

    eu1

--- dispensador_python/menu.py
+++ dispensador_python/menu.py
@@ -7,7 +7,7 @@ menu = f'''
 =========[Dispensadores]=============================
 || {Back.GREEN}1{Back.RESET} - {Fore.CYAN}Dispensar Agua o Comida{Fore.RESET}                      ||
 || {Back.GREEN}2{Back.RESET} - {Fore.MAGENTA}Consulta Contenedores{Fore.RESET}                        ||
-|| {Back.GREEN}3{Back.RESET} - {Fore.BLUE}Insertar Agua o Comida(minuto){Fore.RESET}               ||
+|| {Back.GREEN}3{Back.RESET} - {Fore.BLUE}Dispensar automatico{Fore.RESET}                      ||
 || {Back.GREEN}s{Back.RESET} - {Fore.RED}Salir{Fore.RESET}                                        ||
 ======================================================
 '''
@@ -15,6 +15,13 @@ menu = f'''
 subMenu = f'''
 ||      {Back.GREEN}A.{Back.RESET}{Fore.BLUE} Agua {Fore.RESET}                     ||
 ||      {Back.GREEN}B.{Back.RESET}{Fore.YELLOW} Comida {Fore.RESET}                   ||
+||      {Back.GREEN}M.{Back.RESET}{Fore.RED} Volver al Menú Principal {Fore.RESET} ||
+'''
+
+tiempo = f'''
+{Back.RED}Advertencia: Solo se podrá insertar o enviar una petición mediante una de las siguientes opciones. No se podrá realizar todo al mismo tiempo (minuto, segundo){Back.RESET}
+||      {Back.GREEN}A.{Back.RESET}{Fore.BLUE} Minuto {Fore.RESET}                     ||
+||      {Back.GREEN}B.{Back.RESET}{Fore.YELLOW} Segundo {Fore.RESET}                   ||
 ||      {Back.GREEN}M.{Back.RESET}{Fore.RED} Volver al Menú Principal {Fore.RESET} ||
 '''
 
@@ -30,13 +37,20 @@ while True:
         if opcion_secundaria.lower() == 'm':
             continue  # Regresar al menú principal
 
-        if opcion_principal == '1':
-            print(f'Abrir dispensador de {Fore.BLUE}{opcion_secundaria}{Fore.RESET}')
-        elif opcion_principal == '2':
-            print(f'{Fore.MAGENTA}Consultar dispensador de {Fore.BLUE}{opcion_secundaria}{Fore.RESET}')
-        elif opcion_principal == '3':
-            print(f'{Back.RED}Advertencia: Solo se podrá insertar o enviar una petición mediante una de las siguientes opciones. No se podrá realizar todo al mismo tiempo (Hora, minuto, segundo){Back.RESET}')
-            print(f'Consultar tiempo del dispensador de {Fore.BLUE}{opcion_secundaria}{Fore.RESET}')
+        elif opcion_secundaria.lower() == 'a' or opcion_secundaria.lower() == 'b':
+            print(tiempo)
+            option_tiempo = input(
+                f"{Fore.GREEN}Seleccione una opción (A-B-M):{Fore.RESET}")
+
+            if option_tiempo.lower() == 'm':
+                continue  # Regresar al menú principal
+
+            if opcion_principal == '1':
+                print(f'Abrir dispensador de {Fore.BLUE}{opcion_secundaria}{Fore.RESET} cada {Fore.BLUE}{Fore.RESET}')
+            elif opcion_principal == '2':
+                print(f'{Fore.MAGENTA}Consultar dispensador de {Fore.BLUE}{opcion_secundaria}{Fore.RESET} cada {Fore.BLUE}{Fore.RESET}')
+            elif opcion_principal == '3':
+                print(f'Consultar tiempo del dispensador de {Fore.BLUE}{opcion_secundaria}{Fore.RESET} cada {Fore.BLUE}{option_tiempo}{Fore.RESET}')
 
     elif opcion_principal.lower() == 's':
         print("Saliendo del programa. ¡Hasta luego!")
