a85dc1fa913f00c35a57deea1dd0b47736bf6e42
EmmanuelOZ <davidozuna84@hotmail.com>
2024-03-09 13:55:22 -0600

    vista del dashboard implementada

--- dispensador_python/vista.py
+++ dispensador_python/vista.py
@@ -8,12 +8,70 @@ class Inicio(Frame):
         super().__init__(master)
         self.master = master
         self.pack()
+        self.var_dispensar_agua = IntVar()
+        self.var_dispensar_comida = IntVar()
+        self.estado_dispensador_agua = StringVar()
+        self.estado_dispensador_comida = StringVar()
+        self.contenedor_agua = StringVar()
+        self.contenedor_comida = StringVar()
+
         self.interfaz()
 
     def interfaz(self):
         self.label = Label(
             self, text="..:: Encender Dispensador ::..")
         self.label.pack(pady=10)
+        self.msg_estados()
+        self.imagenes()
+        self.mis_checkbox()
+
+    def msg_estados(self):
+        # barra de estado(servo)
+        Label(self, fg="blue", font=("Courier New", 10, "bold"),
+              text=self.estado_dispensador_agua.get()).place(x=100, y=40)
+
+        Label(
+            self, fg="green", font=("Courier New", 10, "bold"), text=self.estado_dispensador_comida.get()).place(x=400, y=40)
+
+        Label(
+            self, text=self.contenedor_agua.get(), fg="blue", font=("Courier New", 14, "bold")).place(x=30, y=500)
+
+        Label(self, text=self.contenedor_comida.get(), fg="green",
+              font=("Courier New", 14, "bold")).place(x=30, y=480)
+
+    def imagenes(self):
+        self.imagen_agua = PhotoImage(file="imagen/agua.png")
+        self.imagen_comida = PhotoImage(file="imagen/comida.png")
+        # imagen de agua
+        self.im_agua = Label(self, image=self.imagen_agua)
+        self.im_agua.place(x=70, y=100)
+
+        # imagen de comida
+        self.im_comida = Label(self, image=self.imagen_comida)
+        self.im_comida.place(x=370, y=100)
+
+    def mis_checkbox(self):
+        self.check_agua = Checkbutton(
+            self, text="Dispensar Agua", variable=self.var_dispensar_agua, onvalue=1, offvalue=0)
+        self.check_agua.place(x=100, y=300)
+
+        self.check_comida = Checkbutton(
+            self, text="Dispensar Comida", variable=self.var_dispensar_comida, onvalue=1, offvalue=0)
+        self.check_comida.place(x=400, y=300)
+
+    def set_estado_agua(self, estado, msg):
+        self.var_dispensar_agua.set(estado)
+        self.estado_dispensador_agua.set(msg)
+
+    def set_estado_comida(self, estado, msg):
+        self.var_dispensar_comida.set(estado)
+        self.estado_dispensador_comida.set(msg)
+
+    def set_contenedor_agua(self, estado):
+        self.contenedor_agua.set(estado)
+
+    def set_contenedor_comida(self, estado):
+        self.contenedor_comida.set(estado)
 
 
 class Tiempo(Frame):
@@ -27,16 +85,6 @@ class Tiempo(Frame):
         self.label = Label(
             self, text="..:: Configurar Tiempo ::..")
         self.label.pack(pady=10)
-
-        self.imagen_agua = PhotoImage(file="imagen/agua.png")
-        self.imagen_comida = PhotoImage(file="imagen/comida.png")
-        # imagen de agua
-        self.im_agua = Label(self, image=self.imagen_agua)
-        self.im_agua.place(x=70, y=100)
-
-        # imagen de comida
-        self.im_comida = Label(self, image=self.im_comida)
-        self.im_comida.place(x=370, y=100)
 
 
 class Registro(Frame):
