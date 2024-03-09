dab597d59c2c8b4b1d22404977e054a843df2378
EmmanuelOZ <davidozuna84@hotmail.com>
2024-03-08 21:48:07 -0600

    creacion de la clase estatica ControllerWDispenser

---
+++ src/ControllerWDispenser.cpp
@@ -0,0 +1,49 @@
+#include "ControllerWDispenser.h"
+
+void ControllerWDispenser::setup(String command, byte pin, Sonares &sonar)
+{
+    ControllerWDispenser::sonar = sonar;
+    ControllerWDispenser::command = command;
+    ControllerWDispenser::pin = pin;
+}
+
+void ControllerWDispenser::processCommand(String value)
+{
+    if (value == "0")
+    {
+        if (digitalRead(pin) == HIGH)
+        {
+            digitalWrite(pin, LOW);
+            Serial.println(command + "R:" + String(digitalRead(pin)));
+        }
+        else
+        {
+            Serial.println(command + "R:-1");
+        }
+    }
+    else if (value == "1")
+    {
+        if (digitalRead(pin) == HIGH)
+        {
+            Serial.println(command + "R:-1");
+        }
+        else if (sonar.isDistanceLimit())
+        {
+            Serial.println(command + "R:sonarLimit");
+        }
+        else
+        {
+            digitalWrite(pin, HIGH);
+            Serial.println(command + "R:" + String(digitalRead(pin)));
+        }
+    }
+    else if (value == "2")
+    {
+        int result = digitalRead(pin);
+        Serial.println(command + "P:" + String(result));
+    }
+    else
+    {
+        Serial.println(command + ":notFound");
+    }
+}
---
+++ src/ControllerWDispenser.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include "Sonares.h"
+
+#ifndef CONTROLLER_W_DISPENSER_H
+#define CONTROLLER_W_DISPENSER_H
+
+class ControllerWDispenser
+{
+private:
+    static String command;
+    static byte pin;
+    static Sonares &sonar;
+
+public:
+    static void setup(String command, byte pin, Sonares &sonar);
+    static void processCommand(String value);
+};
+
+#endif // CONTROLLER_TIME_DISPENSER_H
